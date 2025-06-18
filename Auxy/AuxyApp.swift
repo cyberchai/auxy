4957d9678c9802967e87330a05037b834c752925
cyberchai <chairachananharder@gmail.com>
2025-06-18 01:23:26 -0400

    Initial Commit

---
+++ Auxy/AuxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuxyApp.swift
+//  Auxy
+//
+//  Created by Chaira Harder on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AuxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
